6843a561e26da2122a2196482bd6cb1fe25d9977
Asociación Internacional de Ciberseguridad <152721996+Asociacion-Internacional-Ciberseguridad@users.noreply.github.com>
2024-10-25 13:02:37 -0500

    Update demo22.py

--- nivel-intermedio/demo22.py
+++ nivel-intermedio/demo22.py
@@ -1,1 +1,7 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Ejemplo de argparse")
+parser.add_argument('numero', type=int, help="Número de entrada")
+args = parser.parse_args()
+print(f"El número ingresado es {args.numero}")
 
