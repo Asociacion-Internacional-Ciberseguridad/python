0e3c9b458178ffadce6eaf23ae7bc037aa657619
Asociación Internacional de Ciberseguridad <152721996+Asociacion-Internacional-Ciberseguridad@users.noreply.github.com>
2024-10-25 12:29:49 -0500

    Create demo02.py

---
+++ nivel-intermedio/demo02.py
@@ -0,0 +1,10 @@
+class Animal:
+    def hacer_sonido(self):
+        print("El animal hace un sonido.")
+
+class Perro(Animal):
+    def hacer_sonido(self):
+        print("El perro ladra.")
+
+perro = Perro()
+perro.hacer_sonido()
